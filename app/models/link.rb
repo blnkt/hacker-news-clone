2ff3e7b4a50432831c92171da468b346b2ccbda1
Blake Mason & moof mayeda <student@epicodus.com>
2014-08-27 11:36:16 -0700

    vote method for links

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,9 +3,12 @@ class Link < ActiveRecord::Base
   has_many :comments
   validates :url, :presence => true
 
+  def vote
+  self.update(votes: self.votes + 1)
+  end
+
   private
   def url_checker
     self.url.delete!("http://") if self.url.start_with?("http://")
   end
-
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 describe Link do
   it { should validate_presence_of :url }
   it { should have_many :comments }
+
+  describe '#vote' do
+    it 'votes up a link' do
+      new_link = Link.create(url: 'chancestoriestold.com')
+      new_link.vote
+      expect(new_link.votes).to eq(1)
+    end
+  end
 end
